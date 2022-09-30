6488be3b18b93451c92708a006d07223c1e3c817
I.Safonov <safonovil17@yandex.ru>
2022-09-30 12:06:01 +0300

    Added program

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string[] array = new string[] { "Russia", ":-)", "Denmark", "123", "4321" };
+string[] result = new string[array.Length];
+int symbols = 3;
+int count = 0;
+
+WordSearch(array, result);
+PrintArray(result);
+
+void WordSearch(string[] array, string[] result)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= symbols)
+        {
+            result[count] = array[i];
+        }
+        count++;
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
